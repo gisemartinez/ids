a01503452b6c23670ac6f79c0483bb792fd84e2d
Gise Martinez <gise.cpna@gmail.com>
2014-08-25 16:19:17 -0300

    Cambio de lugar los scripts,para mayor legibilidad

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -10,12 +10,93 @@
 //= require_self
 //= require bootstrap
 
-if (typeof jQuery !== 'undefined') {
-	(function($) {
-		$('#spinner').ajaxStart(function() {
-			$(this).fadeIn();
-		}).ajaxStop(function() {
-			$(this).fadeOut();
-		});
-	})(jQuery);
-}
+$(document).ready(function(){
+
+	//
+	//bootstrap
+	//
+
+	//a todos los inputs (menos a los submits) le agrego la clase form-control
+
+	$('input,select').addClass("form-control")
+
+	$('input:checkbox').removeClass("form-control")
+	$('input:submit').removeClass("form-control")
+
+	$('.fieldcontain').removeClass("fieldcontain")
+	//a todas las tablas le agrego la clase table
+	$('table').addClass("table table-bordered")
+	//agrego iconos a los botones
+	// $('a.home').prepend("<span class='glyphicon glyphicon-home'>&nbsp</span>")
+	
+	// $('a.usuarios').prepend("<span class='glyphicon glyphicon-user'>&nbsp</span>")
+
+
+	$('.save').prepend("<i align='center' class='glyphicon glyphicon-ok'></i>")
+	$('.search').append("<i align='center' class='glyphicon glyphicon-search'></i>")
+	$('.delete').append("<i align='center' class='glyphicon glyphicon-trash'></i>")
+	$('a.edit').prepend("<span align='center' class='glyphicon glyphicon-pencil'></span>")
+	$('a.cancel').prepend("<span align='center' class='glyphicon glyphicon-remove'></span>")
+
+
+	$('a.list').prepend("<span class='glyphicon glyphicon-book'>&nbsp</span>")
+	$('a.new').prepend("<span class='glyphicon glyphicon-upload'>&nbsp</span>")
+	$('.perfil a').first().prepend("<span class='glyphicon glyphicon-user'>&nbsp</span>")
+
+	//
+	//Evitar comportamiento extraño de grails
+	//
+
+	//Si no hay suficientes elementos para paginar..
+	if($('ul.pagination li.disabled')){
+		$('div.pagination').css('visibility','hidden')	
+	}
+	//Si no hay elementos para mostrar..
+	if(!$('tbody').children().length){
+		$('div#list-bien').css('visibility','hidden')	
+	}
+	if(!$('tbody').children().length){
+		$('div#list-usuario').css('visibility','hidden')	
+	}
+	//Si hay un input de id=password ,cambiarlo a tipo pass(q no muestre lo que escribe)
+	$('input#password').attr('type','password')
+
+	//Agrego algunos botones al menu dependiendo en que pagina estoy.
+	if(location.pathname=="/patronus/bien/index"){
+		$('div#menu').append("<a class='btn btn-default' href='/patronus/bien/create'>Upload</a>")
+	}
+	if(location.pathname=="/patronus/bien/create"){
+		$('div#menu').append("<a class='btn btn-default' href='/patronus/bien/index'>Bienes</a>")
+	}
+	if(location.pathname.indexOf("/patronus/bien/show")!=-1){
+		$('div#menu').append("<a class='btn btn-default' href='/patronus/bien/index'>Bienes</a>")
+	}
+	
+
+	$(".save").addClass("btn btn-success")
+	$("a.cancel").addClass("btn btn-danger")
+	$(".delete").addClass("btn btn-danger")
+
+	$(".bienes").mouseover(function(){
+		$(".bienes").addClass("open");
+	});
+	$(".bienes").mouseout(function(){
+		$(".bienes").removeClass("open");
+	});
+
+
+	$(".usuarios").mouseover(function(event){
+	$(".usuarios").addClass("open");
+	});
+	$(".usuarios").mouseout(function(event){
+		$(".usuarios").removeClass("open");
+	});
+
+	$(".perfil").mouseover(function(event){
+	$(".perfil").addClass("open");
+	});
+	$(".perfil").mouseout(function(event){
+		$(".perfil").removeClass("open");
+	});
+
+})
