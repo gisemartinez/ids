0d744a7bef2cb60f315fc30ee74403df03ee1c48
Gise Martinez <gise.cpna@gmail.com>
2014-12-30 13:29:25 -0300

    Bootstrap se invoca desde el servidor de la aplicación,ya no por url

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -4,11 +4,10 @@
 //
 // You're free to add application-wide JavaScript to this file, but it's generally better 
 // to create separate JavaScript files as needed.
-//
 //= require jquery
-//= require_tree .
 //= require_self
-//= require bootstrap
+
+//= require_tree bootstrap 
 
 $(document).ready(function(){
 
