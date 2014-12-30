3a22deaf9bf29e606f1169f2e5f68d65f4792143
Gise Martinez <gise.cpna@gmail.com>
2014-12-30 13:35:05 -0300

    Actualización a Bootstrap v 3.3.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -71,7 +71,7 @@ grails.project.dependency.resolution = {
         runtime ":hibernate4:4.3.5.3" // or ":hibernate:3.6.10.15"
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
-        runtime ':twitter-bootstrap:3.1.1.3'
+        runtime ':twitter-bootstrap:3.3.1'
 
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.7.4"
