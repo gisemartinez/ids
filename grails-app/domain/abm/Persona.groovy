313d5e8c79cfcc23b38350ca8b59e038d1d8251f
gisemartinez <gise.cpna@gmail.com>
2015-01-28 15:06:30 -0300

    Metodo de clase filtroPorRol(rol) que trae un listado de personas que tiene el rol especificado

--- grails-app/domain/abm/Persona.groovy
+++ grails-app/domain/abm/Persona.groovy
@@ -11,6 +11,15 @@ class Persona {
 	Date fechaAlta=new Date()
 
 	Date fechaBaja=new Date()
+	//metodo de clase que se llama igual que Persona.list
+	//Si se le da el id del rol que se necesita, filtra por esa categoría
+	static ArrayList filtrarPorRol(idRol){
+	      def rol=idRol
+	      def listaUsuarios = com.testapp.UserRole.findAll("from UserRole where role_id=?",[rol]).userId
+	      def listaPersonas = abm.PersonaUser.findAllByUserId(listaUsuarios).personaId
+	      return Persona.findAllById(listaPersonas)
+	}
+
 	
 	static belongsTo= [area:Area]
 
