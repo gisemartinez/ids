6e4c2df9fab70c6697319e8d06af3023204b388a
ayestaranguillermo <ayestaranguillermo@gmail.com>
2015-02-04 14:16:18 -0300

    Issue #6: Mensaje en listados vacíos
    Limpieza general tmb.

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -15,63 +15,44 @@ $(document).ready(function(){
 	//bootstrap
 	//
 
-	$('th.sortable a').attr('title','Cambiar orden')
-	$('.list-group').attr('style','margin-bottom:0')
-
-
 	//a todos los inputs (menos a los submits) le agrego la clase form-control
 
 	$('input,select,textarea').addClass("form-control")
 	$('input:checkbox').removeClass("form-control")
 	$('input:submit').removeClass("form-control")
-	$('.form-control').attr("style","margin-bottom:10px")
 	$('.fieldcontain').removeClass("fieldcontain")
 
-	//a todas las tablas le agrego la clase table
+	$('.list-group').attr('style','margin-bottom:0')
+	$('.form-control').attr("style","margin-bottom:10px")
+
+	//Tablas
 	$('table').addClass("table table-hover")
 	$('table').wrap('<div class="panel panel-default table-responsive">')
 	$('th,td').attr("style", "text-align:center")
 	$('th a').attr("style", "color:white;text-decoration:none")
-
-
-	$('.barra_principal ul').attr("style", "background-color:#009688")
-	$('.barra_principal a').attr("style", "color:white;background-color:#009688;padding:15px 10px;")
-
-	$('.barra_acciones a').attr("style","padding:5px 5px;")
-
-	$('.navbar-right ul li').attr("style","text-align: right;")
-
-	//$('.barra_acciones .navbar-right ul').attr("style","text-align: right;max-width:50px")
-
-	//
-	//Evitar comportamiento extra&#241o de grails
-	//
+	$('th.sortable a').attr('title','Cambiar orden')
+	
+	//Si no hay elementos para mostrar..
+	if(!$('tbody').children().length){
+		$('.table-responsive').before('<div class="alert alert-warning" role="alert"><i class="md md-info"></i> No hay datos cargados. <a class="alert-link" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample" id="accion">Desea agregar uno?</a></div>')
+		$('.table-responsive').css('visibility','hidden')
+	}
 
 	//Si no hay suficientes elementos para paginar..
 	if($('ul.pagination li.disabled')){
 		$('div.pagination').css('visibility','hidden')	
 	}
-	//Si no hay elementos para mostrar..
-	if(!$('tbody').children().length){
-		$('.table-responsive').css('visibility','hidden')	
-	}
+
+	$('.barra_principal ul').attr("style", "background-color:#009688")
+	$('.barra_principal a').attr("style", "color:white;background-color:#009688;padding:15px 10px;")
+	$('.barra_acciones a').attr("style","padding:5px 5px;")
+	$('.navbar-right ul li').attr("style","text-align: right;")
+
+	$('.barra_principal .navbar-right ul a').attr("style","padding:3px 20px;color:white;background-color:#009688;")
+	$('.barra_acciones .navbar-right ul a').attr("style","padding:3px 20px;")
+
 	//Si hay un input de id=password ,cambiarlo a tipo pass(q no muestre lo que escribe)
 	$('input#password').attr('type','password')
-
-	//Agrego algunos botones al menu dependiendo en que pagina estoy.
-
-	if(location.pathname=="/${grailsApplication.config.nombreAplicacion}/bien/index"){
-		$('div#menu').append("<a class='btn btn-default' href='/${grailsApplication.config.nombreAplicacion}/bien/create'>Upload</a>")
-	}
-	if(location.pathname=="/${grailsApplication.config.nombreAplicacion}/bien/create"){
-		$('div#menu').append("<a class='btn btn-default' href='/${grailsApplication.config.nombreAplicacion}/bien/index'>Bienes</a>")
-	}
-	if(location.pathname.indexOf("/${grailsApplication.config.nombreAplicacion}/bien/show")!=-1){
-		$('div#menu').append("<a class='btn btn-default' href='/${grailsApplication.config.nombreAplicacion}/${grailsApplication.config.nombreAplicacion}/bien/index'>Bienes</a>")
-	}
-	
-
-
 
 	drop_up($(".estado"))
 	drop_up($(".perfil"))
@@ -102,42 +83,5 @@ $(document).ready(function(){
 			elem.addClass("open")
 		})
 	}
-
-	$("#nombre").change(function(e){
-		var txt = $("#nombre").val();
-		validarLetras( txt );
-	})
-
-	$("#apellido").change(function(e){
-		var txt = $("#apellido").val();
-		validarLetras( txt );
-	})
-
-	$("#codigoDeSerie").change(function(e){
-		var txt = $("#codigoDeSerie").val();
-		validarNumeros( txt );
-	})
-
+	
 });
-
-
-
-function validarLetras( txt ){
-	if( !esSoloLetras(txt) )
-		alert( "Debe ingresar solo letras" );
-}
-
-function esSoloLetras( txt ){
-		var re = /^([a-z]|[A-Z]|\s)+$/;
-		return re.test(txt);	
-}
-
-function esSoloNumero( txt ){
-		var nu = /^([0-9])+$/;
-		return nu.test(txt);	
-}
-
-function validarNumeros( txt ){
-	if( !esSoloNumero(txt) )
-		alert( "Debe ingresar solo numeros" );
-}
