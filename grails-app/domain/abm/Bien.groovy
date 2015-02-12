b2667306867633a315a198bf76d66abbb9eccfb9
ayestaranguillermo <ayestaranguillermo@gmail.com>
2015-02-12 18:24:12 -0300

    Clase Auditable

--- grails-app/domain/abm/Bien.groovy
+++ grails-app/domain/abm/Bien.groovy
@@ -3,30 +3,30 @@ package abm
 import java.util.Date;
 
 class Bien {
+	static auditable = true
 
-       String codigoPatrimonio
+	String codigoPatrimonio
 
-       String nombreBien
+	String nombreBien
 
 	String codigoDeSerie
 
-       String descripcion
-       //relacion de tablas
-       static belongsTo= [estado:Estado,area:Area,ubicacion: Ubicacion,tipo:Tipo,responsableBien:Persona]
+	String descripcion
+	//relacion de tablas
+	static belongsTo= [estado:Estado,area:Area,ubicacion: Ubicacion,tipo:Tipo,responsableBien:Persona]
       
-       //Fecha del alta del bien
-       Date fechaAlta=new Date()
-       //Fecha de baja del bien-por default: 31/12/2999
-       Date fechaBaja=new Date('2999/12/31')
+	//Fecha del alta del bien
+	Date fechaAlta=new Date()
+	//Fecha de baja del bien-por default: 31/12/2999
+	Date fechaBaja=new Date('2999/12/31')
                
-       static constraints = {
-       		
-       }
-       static mapping = {
-              responsableBien lazy: false
-              tipo lazy: false
-              ubicacion lazy: false
-              area lazy: false
-              estado lazy: false
-       }
+	static constraints = {
+	}
+	static mapping = {
+		responsableBien lazy: false
+		tipo lazy: false
+		ubicacion lazy: false
+		area lazy: false
+		estado lazy: false
+	}
 }
