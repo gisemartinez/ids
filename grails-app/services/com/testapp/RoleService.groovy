5c01b65455eb28f2cfdaf76a1062088312266af9
ayestaranguillermo <ayestaranguillermo@gmail.com>
2015-05-08 03:48:47 -0300

    #17: Solucion Inicial.
    Asi como esta cada rol puede cargar personas con rol por debajo del suyo.

--- grails-app/services/com/testapp/RoleService.groovy
+++ grails-app/services/com/testapp/RoleService.groovy
@@ -1,11 +1,33 @@
 package com.testapp
 
 import grails.transaction.Transactional
+import com.testapp.User
+import grails.plugin.springsecurity.annotation.Secured
+import grails.plugin.springsecurity.SpringSecurityService
 
 @Transactional
 class RoleService {
 
-    def serviceMethod() {
+	def springSecurityService
 
+	def listado(){
+		def ROLE_SUPERVISOR = "ROLE_SUPERVISOR"
+		def ROLE_ENCARGADO = "ROLE_ENCARGADO"
+		def ROLE_OPERARIO = "ROLE_OPERARIO"
+		def rol = Role.permisoSesionActual(User.idRolSesionActual(springSecurityService.loadCurrentUser().id))
+		switch(rol){
+			case ROLE_SUPERVISOR:
+				return Role.where{
+					authority == ROLE_ENCARGADO ||
+					authority == ROLE_OPERARIO
+				}.findAll();
+			break;
+			case ROLE_ENCARGADO:
+				return Estado.where{
+					authority == ROLE_OPERARIO
+				}.findAll();
+			break;
+			case ROLE_OPERARIO:
+			break;
+		}
     }
-}
