c6cc76e63cc8346fb7473022049c7a6e1edfe67b
ayestaranguillermo <ayestaranguillermo@gmail.com>
2015-08-16 04:32:31 -0300

    Update UrlMappings.groovy

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,19 +1,18 @@
 class UrlMappings {
 
 	static mappings = {
-        
-       
-      
+          
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
-              
             }
         }
-    
+        
+        "/index"(view:"/index")
         "/"(view:"/index")
         "500"(view:'/error')
         "/permisos"(view:"/permisos")
         "/qrcode"(view:"/qrcode")
+        "/mdl"(view:"/mdl")
 	}
 }
