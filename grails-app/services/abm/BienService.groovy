820f3421aeb6fe1d421462e95dd95547a36c5fe1
ayestaranguillermo <ayestaranguillermo@gmail.com>
2015-08-16 04:21:05 -0300

    Update BienService.groovy

--- grails-app/services/abm/BienService.groovy
+++ grails-app/services/abm/BienService.groovy
@@ -52,12 +52,12 @@ class BienService {
         else {
             def usuario = userService.userSesionActual().username
             def cuerpoMail = "El usuario "+usuario+" ha intentado modificar el bien: "+bienInstance.descripcion+".\ncodigoPatrimonio: " +bienInstance.codigoPatrimonio+"\nnombreBien: "+bienInstance.nombreBien+"\ncodigoDeSerie: "+bienInstance.codigoDeSerie+"\ndescripcion: "+bienInstance.descripcion+"\nresponsableBien: "+bienInstance.responsableBien+"\ntipo: "+bienInstance.tipo+"\nubicacion: "+bienInstance.ubicacion.nombreubica+"\narea: "+bienInstance.area.nombrearea+"\nestado: "+bienInstance.estado.nombre+"\n"
-            //this.enviarMail(cuerpoMail,GUILLE)
+
             println(cuerpoMail)
             bienInstance.discard()
+            //this.enviarMail(cuerpoMail,GUILLE)
             return
         }
-
     }
     def borrar(bienInstance){
     	if (bienInstance == null) {
@@ -69,68 +69,67 @@ class BienService {
     def enviarMail(String contenidoMail, String destinatario){
     	sendMail {
            to destinatario
-           cc GISE,GUILLE,ROMI,NAIR
+           //cc GISE,GUILLE,ROMI,NAIR
            from PATRONUS
            subject "Patronus"
            text contenidoMail   
         }
     }
-    def buscarBienesPorQuery(query){
-    	if (roleService.nombreDelRolDeSesionActual() == roleService.getSupervisor())
-            return Bien.findAll("from Bien where INSTR(nombreBien,?)>0",[query])
-        else{
-            def areaUser = Persona.findById(personaService.getIdPersonaSesionActual( userService.idUserSesionActual() )).area
-            return Bien.findAll("from Bien where INSTR(nombreBien,?)>0 and area_id = ?",[query,areaUser])
+    def getBienesSegunPermiso(max, offset, query){
+        // si el rol es Admin, traigo todos los bienes.En caso contrario, traigo los bienes que correspondan al area de la persona
+        if (max == 0) {
+            if (roleService.nombreDelRolDeSesionActual() == roleService.getSupervisor())
+                return Bien.findAll("from Bien where (INSTR(nombreBien,?)>0 or INSTR(descripcion,?)>0 or INSTR(codigoPatrimonio,?)>0 or INSTR(codigoDeSerie,?)>0 or INSTR(fechaAlta,?)>0))",[query,query,query,query,query])
+            else {
+                def areaUser = Persona.findById(personaService.getIdPersonaSesionActual( userService.idUserSesionActual() )).area
+                return Bien.findAll("from Bien where (INSTR(nombreBien,?)>0 or INSTR(descripcion,?)>0 or INSTR(codigoPatrimonio,?)>0 or INSTR(codigoDeSerie,?)>0 or INSTR(fechaAlta,?)>0)) and area_id = ?",[query,query,query,query,query,areaUser])
+            }
+        }
+        else {
+            if (roleService.nombreDelRolDeSesionActual() == roleService.getSupervisor())
+                return Bien.findAll("from Bien where (INSTR(nombreBien,?)>0 or INSTR(descripcion,?)>0 or INSTR(codigoPatrimonio,?)>0 or INSTR(codigoDeSerie,?)>0  or INSTR(fechaAlta,?)>0))",[query,query,query,query,query],[max: max, offset: offset.toInteger()])
+            else {
+                def areaUser = Persona.findById(personaService.getIdPersonaSesionActual( userService.idUserSesionActual() )).area
+                return Bien.findAll("from Bien where (INSTR(nombreBien,?)>0 or INSTR(descripcion,?)>0 or INSTR(codigoPatrimonio,?)>0 or INSTR(codigoDeSerie,?)>0 or INSTR(fechaAlta,?)>0)) and area_id = ?",[query,query,query,query,query,areaUser],[max: max, offset: offset.toInteger()])
+            }
         }
     }
-    def mostrarBienesSegunPermiso(){
-        //--si el rol es Admin, traigo todos los bienes.En caso contrario,
-        //traigo los bienes que correspondan al area de la persona
-        if (roleService.nombreDelRolDeSesionActual() == roleService.getSupervisor())
-            return Bien.findAll()
-        else{
-            def areaUser = Persona.findById(personaService.getIdPersonaSesionActual( userService.idUserSesionActual() )).area
-            return Bien.findAllByArea(areaUser)
-        }
-    }
+
      def bienesSegunEstado(ArrayList listadoBienes, String nombreEstado){
         def estado = Estado.findByNombre(nombreEstado)
         return listadoBienes.findAll{it.estado.id == estado.id}
     }
     def bienesAEvaluar(){
-        def listaAE = this.bienesSegunEstado(this.mostrarBienesSegunPermiso(), estadoService.getEstadoAEvaluar())
+        def listaAE = this.bienesSegunEstado(this.getBienesSegunPermiso(0,0,""), estadoService.getEstadoAEvaluar())
         return listaAE
     }
 
     def bienesEnUso(){
-        def listaEU = this.bienesSegunEstado(this.mostrarBienesSegunPermiso(), estadoService.getEstadoEnUso())
+        def listaEU = this.bienesSegunEstado(this.getBienesSegunPermiso(0,0,""), estadoService.getEstadoEnUso())
         return listaEU
     }
 
     def bienesAReparar(){
-        def listaAR = this.bienesSegunEstado(this.mostrarBienesSegunPermiso(), estadoService.getEstadoAReparar())
+        def listaAR = this.bienesSegunEstado(this.getBienesSegunPermiso(0,0,""), estadoService.getEstadoAReparar())
         return listaAR
     }
     
     def bienesADonacion(){
-        def listaAD = this.bienesSegunEstado(this.mostrarBienesSegunPermiso(),  estadoService.getEstadoADonacion())
+        def listaAD = this.bienesSegunEstado(this.getBienesSegunPermiso(0,0,""),  estadoService.getEstadoADonacion())
         return listaAD
     }
 
     def bienesADescarte(){
-        def listaADs = this.bienesSegunEstado(this.mostrarBienesSegunPermiso(), estadoService.getEstadoADescarte())
+        def listaADs = this.bienesSegunEstado(this.getBienesSegunPermiso(0,0,""), estadoService.getEstadoADescarte())
         return listaADs
     }
 
     def bienesBaja(){
-        def listaB = this.bienesSegunEstado(this.mostrarBienesSegunPermiso(), estadoService.getEstadoDeBaja() )
+        def listaB = this.bienesSegunEstado(this.getBienesSegunPermiso(0,0,""), estadoService.getEstadoDeBaja() )
         return listaB
     }
 
     def ordenarLista(ArrayList lista, String ordenarPor, String orden){
-        //Seteo al codigo de serie como critero de ordenamiento por defecto 
-        if (ordenarPor == null) {ordenarPor = "codigoDeSerie"}
-        //ordeno
         return lista.sort{
             a, b -> 
                 if (orden == 'desc') {
