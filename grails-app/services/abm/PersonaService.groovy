534f0164b9c2949fc1a8cf5a771b988f2aa93694
ayestaranguillermo <ayestaranguillermo@gmail.com>
2015-10-11 10:54:35 -0300

    Update PersonaService.groovy

--- grails-app/services/abm/PersonaService.groovy
+++ grails-app/services/abm/PersonaService.groovy
@@ -17,12 +17,17 @@ class PersonaService {
     def USUARIO_IMPOSIBLE_DE_BORRAR = "El usuario que intenta borrar es el usuario dueño de ésta sesión"
     def TIENE_MISMO_ROL = "El usuario que intenta borrar posee el mismo rol que ústed"
 
+    def getNombrePersonaSesionActual(){
+        def persona = Persona.findById(getIdPersonaSesionActual())
+        return (persona.nombre +" "+ persona.apellido)
+    }
+
     def getPersonasSegunPermiso(int max, offset, query){
         if (max == 0) {
             if (roleService.nombreDelRolDeSesionActual() == roleService.getSupervisor())
                 return Persona.findAll("from Persona where (INSTR(nombre,?)>0 or INSTR(apellido,?)>0 or INSTR(dni,?)>0 or INSTR(fechaAlta,?)>0))",[query,query,query,query])
             else {
-                def areaUser = Persona.findById(personaService.getIdPersonaSesionActual( userService.idUserSesionActual() )).area
+                def areaUser = Persona.findById(personaService.getIdPersonaSesionActual()).area
                 return Persona.findAll("from Persona where (INSTR(nombre,?)>0 or INSTR(apellido,?)>0 or INSTR(dni,?)>0 or INSTR(fechaAlta,?)>0)) and area_id = ?",[query,query,query,query,areaUser])
             }
         }
@@ -30,7 +35,7 @@ class PersonaService {
             if (roleService.nombreDelRolDeSesionActual() == roleService.getSupervisor())
                 return Persona.findAll("from Persona where (INSTR(nombre,?)>0 or INSTR(apellido,?)>0 or INSTR(dni,?)>0 or INSTR(fechaAlta,?)>0))",[query,query,query,query],[max: max, offset: offset.toInteger()])
             else {
-                def areaUser = Persona.findById(personaService.getIdPersonaSesionActual( userService.idUserSesionActual() )).area
+                def areaUser = Persona.findById(personaService.getIdPersonaSesionActual()).area
                 return Persona.findAll("from Persona where (INSTR(nombre,?)>0 or INSTR(apellido,?)>0 or INSTR(dni,?)>0 or INSTR(fechaAlta,?)>0)) and area_id = ?",[query,query,query,query,areaUser],[max: max, offset: offset.toInteger()])
             }
         }
@@ -54,13 +59,15 @@ class PersonaService {
 	      def listaPersonas = PersonaUser.findAll("from UserRole where user_id in (:usuarios)",[usuarios: listaUsuarios]).userId
 	      return Persona.findAll("from Persona where id in (:personas)",[personas: listaPersonas])	      
 	}
+
 	//este método debéría ir en la clase PersonaUser o en User
 	User getUserDePersona(personaInstance){
 		def persona_user = PersonaUser.findByPersonaId(personaInstance.id)
 		return User.findById(persona_user.userId)
 	}
-	long getIdPersonaSesionActual(idUserSesionActual){
-        return  PersonaUser.findByUserId( idUserSesionActual ).personaId
+
+	long getIdPersonaSesionActual(){
+        return PersonaUser.findByUserId(userService.idUserSesionActual()).personaId
     }
     //este método debéría ir en la clase UserRole o Role
 	 
@@ -70,6 +77,7 @@ class PersonaService {
 		def role_id = UserRole.findByUser(user).roleId
 		return Role.findById(role_id)
 	}
+
     def actualizarRolDeUsuario(idRol,userInstance){
         //busco y elimino el par UsuarioRol
         def u_r = UserRole.findByUser(com.testapp.User.findById(userInstance.id))
@@ -80,25 +88,28 @@ class PersonaService {
      }
 
     def crear(Persona personaInstance,User userInstance,idRol){
-
     }
 
 	def guardar(Persona personaInstance,User userInstance,idRol){
 		def roleInstance = Role.findById(idRol)
+
 		if (personaInstance == null) {
             notFound()
             return
         }
+
         if (personaInstance.hasErrors()) {
             //respond personaInstance.errors, view:'create'
             throw new RuntimeException("La Persona"+ SE_HA_GUARDADO_CON_ERRORES, personaInstance.errors)
             return
         }
+
         //--------lo mismo pero con el usuario--------
          if (userInstance == null) {
             notFound()
             return
         }
+
         if (userInstance.hasErrors()) {
             //respond userInstance.errors, view:'create'
             throw new RuntimeException("El usuario"+ SE_HA_GUARDADO_CON_ERRORES, userInstance.errors)
@@ -111,13 +122,13 @@ class PersonaService {
             throw new RuntimeException(CONTRASENIAS_DIFF, userInstance.errors)
             return
         }
+
         personaInstance.save flush:true
         userInstance.save flush:true
         //------por mas que sean IDs diferentes, la relacion se guarda igual
         UserRole.create userInstance, roleInstance, true
         //def relationUserRol = new UserRole(userInstance,roleInstance).save(flush:true)
         def relationPersonaUser = new PersonaUser(personaInstance,userInstance).save(flush:true)
-        
     }
 
     def borrar(personaInstance){
@@ -128,17 +139,15 @@ class PersonaService {
 
         def userInstance = this.getUserDePersona(personaInstance) 
         //el usuario a eliminar es el de la sesión iniciada?
+
         if( userService.idUserSesionActual() == userInstance.id){
             //log.Error "Son usuarios con un mismo rol "
-
             throw new RuntimeException(USUARIO_IMPOSIBLE_DE_BORRAR)
-
         }
+        
         //Comparten el rol?
         if (roleService.roleIgualAlDeLaSesionActual(userInstance.id)){
-
             throw new RuntimeException(TIENE_MISMO_ROL)
-
         } else {
             //tiene permisos para ésta operacion?
             if (roleService.roleEsDeMenorJerarquia(userInstance.id)) {
@@ -150,7 +159,6 @@ class PersonaService {
                 throw new RuntimeException(NO_POSEE_LOS_PERMISOS_NECESARIOS)
             }
         }
-
     }
 
     def actualizar(personaInstance,userInstance,idRol){
