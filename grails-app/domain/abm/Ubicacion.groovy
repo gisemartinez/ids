5dbbf022808900f9e5bf9fdf58ca057dd45f022c
ayestaranguillermo <ayestaranguillermo@gmail.com>
2015-10-11 11:19:54 -0300

    Update Ubicacion.groovy

--- grails-app/domain/abm/Ubicacion.groovy
+++ grails-app/domain/abm/Ubicacion.groovy
@@ -3,12 +3,10 @@ package abm
 import java.util.Date;
 
 class Ubicacion {
+	
 	String nombreubica
-
-	   //Fecha del alta de la ubicacion
-       Date fechaAlta=new Date()
-       //Fecha de baja de la ubicacion-por default: 31/12/2999
-       Date fechaBaja=new Date('2999/12/31')
+	Date fechaAlta=new Date()
+	Date fechaBaja=new Date('2999/12/31')
 	
 	static constraints = {
 	}
@@ -16,4 +14,5 @@ class Ubicacion {
 	String toString(){
 		nombreubica
 	}
+	
 }
