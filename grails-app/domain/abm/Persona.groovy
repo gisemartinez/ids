1c11dcb0c7f0d77d8b99101e276a97cd4b8b27d2
ayestaranguillermo <ayestaranguillermo@gmail.com>
2015-10-11 11:16:23 -0300

    Update Persona.groovy

--- grails-app/domain/abm/Persona.groovy
+++ grails-app/domain/abm/Persona.groovy
@@ -5,20 +5,23 @@ import java.util.Date;
 class Persona {
 	
 	String nombre
-
 	String apellido
-
 	Integer dni
+	Date fechaAlta = new Date()
+	Date fechaBaja = new Date('2999/12/31')
+	static belongsTo = [area:Area]
+	byte[] filePayload
+	String fileType  //Optional
+	String fileName //Optional
 
-	Date fechaAlta=new Date()
+	static constraints = {
+		filePayload(nullable:true, maxSize:1073741824) // max of 4GB file for example
+		fileType(blank:true, nullable:true)
+		fileName(blank:true, nullable:true)
+	}
 
-	Date fechaBaja=new Date('2999/12/31')
-	
-	static belongsTo= [area:Area]
-
-    static constraints = {
-    }
-    String toString(){
+	String toString(){
 		nombre
 	}
+
 }
